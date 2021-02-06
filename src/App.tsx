513ed2bd5f1bd9c5a44814962c57d27cb999ab5e
Isaac Martinez <imtz90b@gmail.com>
2021-02-05 22:51:44 -0600

    Setting up types and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Login } from './containers/Login';
+import { Account } from './containers/Account';
+
+function App(): ReactElement {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/account">Account</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/account">
+                        <Account />
+                    </Route>
+                    <Route path="/">
+                        <Login />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
